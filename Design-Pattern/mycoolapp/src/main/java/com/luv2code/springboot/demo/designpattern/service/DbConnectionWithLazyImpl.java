38426095c895fb18d5a03ffc87c494083ae8abc7
Mohamed shosha <moshosha267@gmail.com>
2024-08-22 17:34:56 +0300

    Update findbyid in Service layer to return student and add getstudentbyid to transformation data of student to studentdto

--- Design-Pattern/mycoolapp/src/main/java/com/luv2code/springboot/demo/designpattern/service/DbConnectionWithLazyImpl.java
+++ Design-Pattern/mycoolapp/src/main/java/com/luv2code/springboot/demo/designpattern/service/DbConnectionWithLazyImpl.java
@@ -29,51 +29,59 @@ public class DbConnectionWithLazyImpl implements DbConnectionService {
     public void addStudent(StudentDto studentDto) {
         String id = UUID.randomUUID().toString();
         Student student = new Student(id, studentDto.getName(), studentDto.getEmail(),
-                studentDto.getAge(), studentDto.getAddress());
+                studentDto.getAge(), studentDto.getAddressDto());
         studentRepository.insertStudent(student);
         log.info("The id of the new student is {} : ", id);
     }
 
     @Override
-    public Collection<StudentDto> getStudents() {
-        return studentRepository.getStudents()
-                .stream()
-                .map(element -> new StudentDto(element.getName(), element.getEmail(), element.getAge(), element.getAddress())).toList();
-
-    }
-
-    @Override
-    public StudentDto findStudentById(String id) throws StudentNotFoundException {
+    public Student findStudentById(String id) throws StudentNotFoundException {
         StringBuilder methodName = new StringBuilder(className + ".findStudentById");
         log.info("Id of student that I will search with is: {}", id);
         Optional<Student> student = studentRepository.findStudentById(id);
         if (student.isPresent()) {
             Object[] parameters;
-            parameters = new Object[]{methodName.toString(), id, student.get().getName(), student.get().getEmail(), student.get().getAge(), student.get().getAddress()};
+            parameters = new Object[]{methodName.toString(), id, student.get().getName(),
+                    student.get().getEmail(), student.get().getAge(), student.get().getAddressDto()};
             log.info("{},I found id and his value is {}, Name of student is {},email is : {} ," +
                     " age is {} and his address is {} ", parameters);
         }
 
         return student.stream()
-                .map(element -> new StudentDto(element.getName(), element.getEmail(),
-                        element.getAge(), element.getAddress()))
+                .filter(element -> element.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() ->
                         new StudentNotFoundException("Student not found"));
     }
 
     @Override
+    public Collection<StudentDto> getStudents() {
+        return studentRepository.getStudents()
+                .stream()
+                .map(element -> new StudentDto(element.getName(), element.getEmail(),
+                        element.getAge(), element.getAddressDto())).toList();
+    }
+
+    @Override
+    public StudentDto getStudent(String id) {
+        Student student = findStudentById(id);
+        return new StudentDto(student.getName(), student.getEmail(),
+                student.getAge(), student.getAddressDto());
+    }
+
+
+    @Override
     public void updateStudent(String id, StudentDto updatedStudent) {
-        StudentDto studentDto = findStudentById(id);
-        studentDto.setName(updatedStudent.getName());
-        studentDto.setEmail(updatedStudent.getEmail());
-        studentDto.setAge(updatedStudent.getAge());
-        studentDto.setAddress(updatedStudent.getAddress());
+        Student student = findStudentById(id);
+        student.setName(updatedStudent.getName());
+        student.setEmail(updatedStudent.getEmail());
+        student.setAge(updatedStudent.getAge());
+        student.setAddress(updatedStudent.getAddressDto());
     }
 
     @Override
     public void deleteStudentById(String id) {
-        StudentDto studentDto = findStudentById(id);
+        Student studentDto = findStudentById(id);
         studentRepository.deleteStudentById(id);
     }
 
@@ -87,7 +95,7 @@ public class DbConnectionWithLazyImpl implements DbConnectionService {
         if (studentRepository.countStudents() > 0) {
             studentRepository.clear();
         } else {
-            throw new IsEmpty("No items to delete","The map is empty");
+            throw new IsEmpty("No items to delete", "The map is empty");
         }
     }
 
